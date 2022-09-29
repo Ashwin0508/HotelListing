da6cb700f97db06d41066eb08decbf3769fd393f
Ashwin0508 <ashwinsaece@gmail.com>
2022-09-30 00:43:45 +0530

    Added Entity FrameWork and Created Database

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options):base(options)
+        {    
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+    }
+}
